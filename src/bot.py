5d41f94297e323dcf92813cecdaf908ae4dbefe1
Zac Smith <Smith7929@gmail.com>
2024-10-27 22:50:29 -0700

    bot v0.1

--- src/bot.py
+++ src/bot.py
@@ -1,22 +1,31 @@
 import discord
+import os
 from discord.ext import commands
+from dotenv import load_dotenv
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-TOKEN = 'YOUR_BOT_TOKEN'
+# Load environment variables from .env
+load_dotenv()
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
-# Define the bot and the command prefix
-bot = commands.Bot(command_prefix='!')
+# Set up intents
+intents = discord.Intents.default()
+intents.message_content = True  # Allows the bot to read message content
 
-# Event: When the bot is ready
+# Set up bot with command prefix
+bot = commands.Bot(command_prefix='!', intents=intents)
+
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
-    print('------')
+    print(f'{bot.user.name} has connected to Discord!')
 
-# Command: Ping
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
+# Example command
+@bot.command(name='start', help='Starts the Factorio server')
+async def start_server(ctx):
+    await ctx.send("Starting the Factorio server...")
 
-# Run the bot
+@bot.command(name='stop', help='Stops the Factorio server')
+async def stop_server(ctx):
+    await ctx.send("Stopping the Factorio server...")
+
+# Run the bot with token
 bot.run(TOKEN)
